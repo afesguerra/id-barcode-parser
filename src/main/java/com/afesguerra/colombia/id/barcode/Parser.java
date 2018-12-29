994191dd91d623a04cdfc00c41057c3b61676d24
Andrés Felipe Esguerra Restrepo <afesguerra@outlook.com>
2018-12-29 21:15:39 +0100

    Hello World

---
+++ src/main/java/com/afesguerra/colombia/id/barcode/Parser.java
@@ -0,0 +1,10 @@
+package com.afesguerra.colombia.id.barcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Parser {
+    public static void main(String[] args) {
+        log.info("Hello Kaos");
+    }
+}
